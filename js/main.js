54c03f7831bc83eeb50d8b6f2de4fd385967f00e
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-07-01 12:59:33 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -3,27 +3,24 @@ jQuery(document).ready(function( $ ) {
         delay: 10,
         time: 1000
     });
-
     $(window).scroll(function(){
         let winheight = $(window).height();
         let scrollhight= window.scrollY;
         let height_from_top = $('.blog h2').offset().top;
         let diff=height_from_top - scrollhight;
+        //Blog heding 2
        if(winheight>diff){
         $('.blog h2').addClass('animate-run');
         $('.blog-about').addClass('animate-run');
         
        }
+       //Blog 
        let blog_con_height = $('.blog-con').offset().top;
        let blog_diff = blog_con_height - scrollhight;
        if(winheight>blog_diff){
         $('.blog-con').addClass('animate-run');
        }
 
-        
     });
-
-    
-
-
-});
+        
+})
